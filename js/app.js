b1fbf1411fc1456dbd7312f25f20e92b11bebda7
Iuliia <iulia.verevkina@mail.ru>
2023-02-23 21:20:03 +0100

    added habit from modal form

--- js/app.js
+++ js/app.js
@@ -17,6 +17,9 @@ const page = {
     daysContainer: document.getElementById("days"),
     nextDay: document.querySelector(".habbit__day"),
   },
+  modal: {
+    modalField: document.querySelector(".modal__form input[name = 'icon']"),
+  },
 };
 
 // utils
@@ -30,6 +33,35 @@ function loadData() {
 
 function saveData() {
   localStorage.setItem(HABBIT_KEY, JSON.stringify(habbits));
+}
+
+function resetForm(form, fields) {
+  for (const field of fields) {
+    form[field].value = '';
+  }
+}
+
+function validateAndGetFormData(form, fields) {
+  const formData = new FormData(form);
+  const res = {};
+  for (const field of fields) {
+    const fieldValue = formData.get(field);
+    form[field].classList.remove("error");
+    if (!fieldValue) {
+      form[field].classList.add("error");
+    }
+    res[field] = fieldValue;
+  }
+  let isValid = true;
+  for (const field of fields) {
+    if(!res[field]) {
+      isValid = false;
+    }
+  }
+  if(!isValid) {
+    return;
+  }
+  return res;
 }
 
 // render
@@ -110,30 +142,26 @@ function rerender(activeHabbitId) {
 
 // work with days
 function addDays(event) {
-  const form = event.target;
   event.preventDefault();
-  const data = new FormData(form);
-  const comment = data.get("comment");
-  form["comment"].classList.remove("error");
-  if (!comment) {
-    form["comment"].classList.add("error");
+  const data = validateAndGetFormData(event.target, ["comment"]);
+  if(!data) {
+    return;
   }
   habbits = habbits.map((habbit) => {
     if (habbit.id === globalActiveHabbitId) {
       return {
         ...habbit,
-        days: habbit.days.concat([{ comment }]),
+        days: habbit.days.concat([{ comment: data.comment }]),
       };
     }
     return habbit;
   });
-  form["comment"].value = "";
+  resetForm(event.target, ["comment"]);
   rerender(globalActiveHabbitId);
   saveData();
 }
 
 // remove Day
-
 function removeDay(index) {
   habbits = habbits.map((habbit) => {
     if (habbit.id === globalActiveHabbitId) {
@@ -147,6 +175,33 @@ function removeDay(index) {
   });
   rerender(globalActiveHabbitId);
   saveData();
+}
+
+// working with habbits
+function setIcon(context, icon) {
+  page.modal.modalField.value = icon;
+  const activeIcon = document.querySelector(".icon.icon__active");
+  activeIcon.classList.remove("icon__active");
+  context.classList.add("icon__active");
+}
+
+function addHabbit(event) {
+   event.preventDefault();
+   const data = validateAndGetFormData(event.target, ["name", "icon", "target"]);
+   if (!data) {
+     return;
+   }
+   const maxId = habbits.reduce((acc, habbit) => acc > habbit.id ? acc : habbit.id, 0)
+   habbits.push({
+    id: maxId + 1,
+    name: data.name,
+    target: data.target,
+    icon: data.icon,
+    days: []
+   });
+   resetForm(event.target, ["name", "target"]);
+   saveData();
+   rerender(maxId + 1);
 }
 
 // init
